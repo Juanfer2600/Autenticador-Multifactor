c62f14ef290e16b568a75a66267c3815257d8d91
Juan Fernando de León Granja <jdeleong40@miumg.edu.gt>
2025-04-02 11:00:04 -0600

    Add initial admin interface layout with header, footer, menubar, and scripts

---
+++ admin/assets/menubar.php
@@ -0,0 +1,24 @@
+<div class="app-menu navbar-menu">
+    <div class="navbar-brand-box">
+        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
+            <i class="ri-record-circle-line"></i>
+        </button>
+    </div>
+
+    <div id="scrollbar">
+        <div class="container-fluid">
+            <div id="two-column-menu">
+            </div>
+            <ul class="navbar-nav" id="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="">
+                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dashboards</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+
+    <div class="sidebar-background"></div>
+</div>
