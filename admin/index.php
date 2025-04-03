820f16b1f1b64af6add963f7b69eed36b0602e2a
Juan Fernando de León Granja <jdeleong40@miumg.edu.gt>
2025-04-03 09:18:53 -0600

    Refactor admin interface styles and enhance password input validation

--- admin/index.php
+++ admin/index.php
@@ -1,54 +1,52 @@
 <?php include 'assets/header.php'; ?>
 
 <body>
-
     <div class="auth-page-wrapper pt-5">
         <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
-            <div class="bg-overlay">
-                
-                <div class="auth-page-content">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="text-center mt-sm-5 mb-4 text-white-50">
-                                    <div>
-                                        <a class="d-inline-block auth-logo">
-                                            <img src="assets/images/logo-light.png" alt="" height="20">
-                                        </a>
-                                    </div>
-                                </div>
+            <div class="bg-overlay"> </div>
+        </div>
+
+        <div class="auth-page-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center mt-sm-5 mb-4 text-white-50">
+                            <div>
+                                <a class="d-inline-block auth-logo">
+                                    <img src="assets/images/logo-light.png" alt="" height="20">
+                                </a>
                             </div>
                         </div>
+                    </div>
+                </div>
 
-                        <div class="row justify-content-center">
-                            <div class="col-md-8 col-lg-6 col-xl-5">
-                                <div class="card mt-4 card-bg-fill">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card mt-4 card-bg-fill">
 
-                                    <div class="card-body p-4">
-                                        <div class="text-center mt-2">
-                                            <h5 class="text-primary">Bienvenido de vuelta!!!</h5>
+                            <div class="card-body p-4">
+                                <div class="text-center mt-2">
+                                    <h5 class="text-primary">Bienvenido de vuelta!!!</h5>
+                                </div>
+                                <div class="p-2 mt-4">
+                                    <form action="">
+
+                                        <div class="mb-3">
+                                            <label for="username" class="form-label">Username</label>
+                                            <input type="text" class="form-control" id="username" placeholder="Enter username">
                                         </div>
-                                        <div class="p-2 mt-4">
-                                            <form action="">
 
-                                                <div class="mb-3">
-                                                    <label for="username" class="form-label">Username</label>
-                                                    <input type="text" class="form-control" id="username" placeholder="Enter username">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label class="form-label" for="password-input">Password</label>
-                                                    <div class="position-relative auth-pass-inputgroup mb-3">
-                                                        <input type="password" class="form-control pe-5 password-input" placeholder="Enter password" id="password-input">
-                                                        <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon material-shadow-none" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
-                                                    </div>
-                                                </div>
-                                                <div class="mt-4">
-                                                    <button class="btn btn-success w-100" type="submit">Sign In</button>
-                                                </div>
-                                            </form>
+                                        <div class="mb-3">
+                                            <label class="form-label" for="password-input">Password</label>
+                                            <div class="position-relative auth-pass-inputgroup mb-3">
+                                                <input type="password" class="form-control pe-5 password-input" placeholder="Enter password" id="password-input">
+                                                <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon material-shadow-none" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
+                                            </div>
                                         </div>
-                                    </div>
+                                        <div class="mt-4">
+                                            <button class="btn btn-success w-100" type="submit">Sign In</button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
