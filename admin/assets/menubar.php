c692cd7e6b8289e5762bf96fdb0d36d468d39ac8
Juan Fernando de León Granja <jdeleong40@miumg.edu.gt>
2025-04-03 16:54:16 -0600

    Update admin interface layout with new vertical design, add light logo, and remove unused logo elements

--- admin/assets/menubar.php
+++ admin/assets/menubar.php
@@ -1,5 +1,14 @@
 <div class="app-menu navbar-menu">
     <div class="navbar-brand-box">
+        <!-- Light Logo-->
+        <a href="home.php" class="logo logo-light">
+            <!-- <span class="logo-sm">
+                <img src="assets/images/umg.png" alt="" height="35">
+            </span> -->
+            <span class="logo-lg">
+                <img src="assets/images/logo-sm.png" height="50" width="175">
+            </span>
+        </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
             <i class="ri-record-circle-line"></i>
         </button>
