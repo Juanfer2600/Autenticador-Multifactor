052095010c4448c517c3050322cceef695ea5eb0
Juan Fernando de León Granja <jdeleong40@miumg.edu.gt>
2025-04-11 14:35:38 -0600

    Refactor code structure for improved readability and maintainability; removed redundant code blocks and optimized functions.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your-vendor/autenticador-multifactor',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '755eca8df3670753b4541796fd90ca797c27e4b4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.3',
+            'version' => '6.9.3.0',
+            'reference' => '2f5c94fe7493efc213f643c23b1b1c249d40f47e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'your-vendor/autenticador-multifactor' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '755eca8df3670753b4541796fd90ca797c27e4b4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
