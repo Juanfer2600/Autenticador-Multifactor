c58c5d28e4f8123c0248455af7f3281099f3c4e9
Juan Fernando de León Granja <jdeleong40@miumg.edu.gt>
2025-05-09 15:35:33 -0600

    Implement OTP token check in navbar and add security notifications

--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -20,10 +20,58 @@ echo "<nav class='main-header navbar navbar-expand $navbarClass'>";
   </li>
 </ul>
 
+<?php
+// Check if OTP token is configured in user's MFA methods
+$hasOTP = false;
+$notificationCount = 0;
+$notificationClass = "danger"; // Default to danger (red) for security issues
+$notificationMessage = "Favor verificar tu cuenta";
+$notificationLink = "two-step.php";
+$notificationIcon = "fas fa-shield-alt";
+$notificationTime = "Ahora";
 
+if (isset($user['metodos_mfa']) && !empty($user['metodos_mfa'])) {
+  if (strpos($user['metodos_mfa'], 'Token OTP') !== false) {
+    $hasOTP = true;
+    $notificationClass = "success";
+    $notificationMessage = "Verificación de dos pasos configurada";
+    $notificationIcon = "fas fa-check-circle";
+  }
+}
+
+// Set notification count if OTP is not configured
+if (!$hasOTP) {
+  $notificationCount = 1;
+}
+?>
 
 <!-- Right navbar links -->
 <ul class="navbar-nav ml-auto">
+  <li class="nav-item dropdown">
+    <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false">
+      <i class="far fa-bell fa-lg"></i>
+      <?php if (!$hasOTP): ?>
+        <span class="badge badge-<?php echo $notificationClass; ?> navbar-badge" style="font-size: 0.6rem; right: 3px; top: 3px;">
+          <?php if ($notificationCount > 0) echo $notificationCount; ?>
+        </span>
+      <?php endif; ?>
+    </a>
+    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
+      <span class="dropdown-item dropdown-header">Notificaciones de Seguridad</span>
+      <div class="dropdown-divider"></div>
+      <?php if (!$hasOTP): ?>
+        <a href="<?php echo $notificationLink; ?>" class="dropdown-item">
+          <p class="text-sm"><i class="<?php echo $notificationIcon; ?> mr-2 text-<?php echo $notificationClass; ?>"></i><?php echo $notificationMessage; ?></p>
+          <span class="float-right text-muted text-sm"><?php echo $notificationTime; ?></span>
+        </a>
+      <?php else: ?>
+        <a href="#" class="dropdown-item">
+          <p class="text-sm"><i class="<?php echo $notificationIcon; ?> mr-2 text-<?php echo $notificationClass; ?>"></i><?php echo $notificationMessage; ?></p>
+          <span class="float-right text-muted text-sm"><?php echo $notificationTime; ?></span>
+        </a>
+      <?php endif; ?>
+    </div>
+  </li>
   <li class="nav-item dropdown user-menu">
     <a class="nav-link dropdown-toggle" data-toggle="dropdown">
       <img src="<?php echo $photoSrc; ?>" class="user-image img-circle" alt="User Image">
